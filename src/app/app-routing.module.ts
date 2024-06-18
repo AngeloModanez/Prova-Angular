78be8c56486f481edf07a675efa11d85aa826dd0
Bananadev3 <angelomodanez30@gmail.com>
2024-06-18 14:06:49 -0300

    Rotas Criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './contatos/contatos.component';
+import { TiposComponent } from './tipos/tipos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contatos', component: ContatosComponent },
+  { path: 'tipos', component: TiposComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
